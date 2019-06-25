b8847ce8238a1c39f205f888b1f18623bdf28a48
aminamos <26092352+aminamos@users.noreply.github.com>
2019-06-25 11:37:24 -0700

    saving progress in lab

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,70 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(array)
+    if (array.collect {|x| x.start_with?("r") == true}.uniq[0]) == true
+        true
+    else
+        false
+    end
+end
+
+def contain_a(array)
+    array.collect {|x| 
+        x.include?("a")
+}
+end
+
+def first_wa(array)
+    array.collect {|x|
+        if x.to_s.start_with?("wa")
+            return x
+        end
+    }
+end
+
+=begin
+def remove_non_strings(array)
+    array.map.with_index {|x, index|
+        if x.class != String
+            array.delete_at(index)
+        end
+    }
+end
+def remove_non_strings(array)
+    array.map.with_index {|x, index|
+        if x.class == String
+            x
+        else
+            array.delete_at(index)
+        end
+    }
+end
+=end
+
+def remove_non_strings(array)
+    array.delete_if {|x| x.class != String}
+end
+
+def count_elements
+end
+
+def merge_data(data1,data2)
+
+end
+
+def find_cool(hash)
+
+end
+
+=begin
+def organize_schools(schools)
+    orgschools = {}
+    schools.collect {|key,value|
+        value.each {|location, result|
+            orgschools[result] = key
+            orgschools.values << key
+        }
+}
+binding.pry
+end
+=end
